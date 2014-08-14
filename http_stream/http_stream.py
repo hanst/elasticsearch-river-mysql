121eac49ae17f7cd73da1e24bd8ce55e05182b36
Hans Tan <hans@tierones.com>
2014-08-14 19:29:45 +0800

    * fix json parse datetime issue * mapping mysql table to ES type

--- http_stream/http_stream.py
+++ http_stream/http_stream.py
@@ -9,14 +9,27 @@ from pymysqlreplication.row_event import *
 
 mysql_settings = {'host': '127.0.0.1', 'port': 3306, 'user': 'root', 'passwd': ''}
 
+def default(obj):
+    """Default JSON serializer."""
+    import calendar, datetime
+
+    if isinstance(obj, datetime.datetime):
+        if obj.utcoffset() is not None:
+            obj = obj - obj.utcoffset()
+    millis = int(
+        calendar.timegm(obj.timetuple()) * 1000 +
+        obj.microsecond / 1000
+    )
+    return millis
+
+
 import json
 import cherrypy
 class Streamer(object):
     def __init__(self):
         self.stream = BinLogStreamReader(connection_settings = mysql_settings,
                                          only_events = [DeleteRowsEvent, WriteRowsEvent, UpdateRowsEvent], blocking = True, resume_stream = True)
-
-
+    
     def index(self):
         cherrypy.response.headers['Content-Type'] = 'text/plain'
         def content():
@@ -25,20 +38,24 @@ class Streamer(object):
                     if isinstance(binlogevent, DeleteRowsEvent):
                         yield json.dumps({
                           "action": "delete",
+                          "type": binlogevent.table,
                           "id": row["values"]["id"]}) + "\n"
                     elif isinstance(binlogevent, UpdateRowsEvent):
                         yield json.dumps({
                           "action": "update",
+                          "type": binlogevent.table,
                           "id": row["after_values"]["id"],
                           "doc": row["after_values"]}) + "\n"
                     elif isinstance(binlogevent, WriteRowsEvent):
                         yield json.dumps({
                           "action": "insert",
+                          "type": binlogevent.table,
                           "id": row["values"]["id"],
-                          "doc": row["values"]}) + "\n"
+                          "doc": row["values"]},default=default) + "\n"
         return content()
 
     index.exposed = True
     index._cp_config = {"response.stream": True}
 
+cherrypy.config.update({'server.socket_port': 8081}) 
 cherrypy.quickstart(Streamer())
